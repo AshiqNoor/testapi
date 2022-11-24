94434ff5ffa3ccaf6c89e0e2ee22d3d4ce829a90
Muhammad Ashiq An Noor <36337823+AshiqNoor@users.noreply.github.com>
2022-11-24 15:46:16 +0600

    Create testjson.js

---
+++ testjson.js
@@ -0,0 +1,308 @@
+{
+    "status": true,
+    "message": "Course with module details!",
+    "data": {
+        "id": 1,
+        "course_title": "Health and safety briefing for respiratory diseases – ePROTECT\r\n",
+        "complete_rate": 0,
+        "status": "active",
+        "course_modules": [
+            {
+                "id": 1,
+                "course_id": 1,
+                "module_title": "MODULE ONE",
+                "status": "active",
+                "module_finish_status": 1,
+                "sub_modules": [
+                    {
+                        "id": 1,
+                        "module_id": 1,
+                        "content_title": "Virus Introduction",
+                        "content_type": "video",
+                        "content_path": "https://who.elearning.rultest.com/uploads/files/1668505725.mp4",
+                        "content_resource": "https://who.elearning.rultest.com/uploads/files/2978505793.docx",
+                        "youtube_path": "https://www.youtube.com/watch?v=mbTB0R4uwo0&list=PL9S6xGsoqIBXpUf3nAybxij4agGexFKZV",
+                        "finish_status": 1
+                    },
+                    {
+                        "id": 2,
+                        "module_id": 1,
+                        "content_title": "About Virus",
+                        "content_type": "presentation",
+                        "content_path": "https://who.elearning.rultest.com/uploads/files/sample_test_ppt.pptx",
+                        "content_resource": null,
+                        "youtube_path": "",
+                        "finish_status": 0
+                    },
+                    {
+                        "id": 3,
+                        "module_id": 1,
+                        "content_title": "What is Virus",
+                        "content_type": "video",
+                        "content_path": "https://who.elearning.rultest.com/uploads/files/1668505725.mp4",
+                        "content_resource": "https://who.elearning.rultest.com/uploads/files/2978505793.docx",
+                        "youtube_path": "https://www.youtube.com/watch?v=D7NxdhWZunE",
+                        "finish_status": 0
+                    }
+                ],
+                "quizes": [
+                    {
+                        "id": 1,
+                        "course_id": 1,
+                        "module_id": 1,
+                        "question_title": "Multiple Question?",
+                        "type": "radio",
+                        "status": "active",
+                        "options": [
+                            {
+                                "id": 1,
+                                "quiz_question_id": 1,
+                                "option_text": "ANswer 1",
+                                "correct_answer": "0"
+                            },
+                            {
+                                "id": 2,
+                                "quiz_question_id": 1,
+                                "option_text": "AC",
+                                "correct_answer": "1"
+                            },
+                            {
+                                "id": 3,
+                                "quiz_question_id": 1,
+                                "option_text": "WA",
+                                "correct_answer": "0"
+                            },
+                            {
+                                "id": 4,
+                                "quiz_question_id": 1,
+                                "option_text": "Ans 4",
+                                "correct_answer": "0"
+                            }
+                        ]
+                    },
+                    {
+                        "id": 2,
+                        "course_id": 1,
+                        "module_id": 1,
+                        "question_title": "Single choice?",
+                        "type": "radio",
+                        "status": "active",
+                        "options": [
+                            {
+                                "id": 5,
+                                "quiz_question_id": 2,
+                                "option_text": "ANswer 1",
+                                "correct_answer": "0"
+                            },
+                            {
+                                "id": 6,
+                                "quiz_question_id": 2,
+                                "option_text": "Wr",
+                                "correct_answer": "0"
+                            },
+                            {
+                                "id": 7,
+                                "quiz_question_id": 2,
+                                "option_text": "AC",
+                                "correct_answer": "1"
+                            },
+                            {
+                                "id": 8,
+                                "quiz_question_id": 2,
+                                "option_text": "Ans 4",
+                                "correct_answer": "0"
+                            }
+                        ]
+                    },
+                    {
+                        "id": 3,
+                        "course_id": 1,
+                        "module_id": 1,
+                        "question_title": "Another Q?",
+                        "type": "radio",
+                        "status": "active",
+                        "options": [
+                            {
+                                "id": 9,
+                                "quiz_question_id": 3,
+                                "option_text": "ANswer 1",
+                                "correct_answer": "0"
+                            },
+                            {
+                                "id": 10,
+                                "quiz_question_id": 3,
+                                "option_text": "Wr",
+                                "correct_answer": "0"
+                            },
+                            {
+                                "id": 11,
+                                "quiz_question_id": 3,
+                                "option_text": "WA",
+                                "correct_answer": "0"
+                            },
+                            {
+                                "id": 12,
+                                "quiz_question_id": 3,
+                                "option_text": "AC",
+                                "correct_answer": "1"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "id": 2,
+                "course_id": 1,
+                "module_title": "MODULE Two",
+                "status": "active",
+                "module_finish_status": 0,
+                "sub_modules": [
+                    {
+                        "id": 4,
+                        "module_id": 2,
+                        "content_title": "Virus Introduction",
+                        "content_type": "video",
+                        "content_path": "https://who.elearning.rultest.com/uploads/files/1668505725.mp4",
+                        "content_resource": "https://who.elearning.rultest.com/uploads/files/2978505793.docx",
+                        "youtube_path": "https://www.youtube.com/watch?v=mbTB0R4uwo0&list=PL9S6xGsoqIBXpUf3nAybxij4agGexFKZV",
+                        "finish_status": 0
+                    },
+                    {
+                        "id": 5,
+                        "module_id": 2,
+                        "content_title": "About Virus",
+                        "content_type": "presentation",
+                        "content_path": "https://who.elearning.rultest.com/uploads/files/1668505725.pptx",
+                        "content_resource": null,
+                        "youtube_path": "",
+                        "finish_status": 0
+                    },
+                    {
+                        "id": 6,
+                        "module_id": 2,
+                        "content_title": "What is Virus",
+                        "content_type": "video",
+                        "content_path": "https://who.elearning.rultest.com/uploads/files/1668505725.mp4",
+                        "content_resource": "https://who.elearning.rultest.com/uploads/files/1668505725.pdf",
+                        "youtube_path": "https://www.youtube.com/watch?v=D7NxdhWZunE",
+                        "finish_status": 0
+                    },
+                    {
+                        "id": 7,
+                        "module_id": 2,
+                        "content_title": "Submodule 4",
+                        "content_type": "video",
+                        "content_path": "https://who.elearning.rultest.com/uploads/files/Hthuopf0K6DWhG1Hdjg0JhVbp-1669203957.mp4",
+                        "content_resource": "https://who.elearning.rultest.com/uploads/files/qYLTzJPoorUM4387O295QH3zV-1669203957.pdf",
+                        "youtube_path": null,
+                        "finish_status": 0
+                    }
+                ],
+                "quizes": [
+                    {
+                        "id": 4,
+                        "course_id": 1,
+                        "module_id": 2,
+                        "question_title": "Another Q?",
+                        "type": "radio",
+                        "status": "active",
+                        "options": [
+                            {
+                                "id": 13,
+                                "quiz_question_id": 4,
+                                "option_text": "option 1",
+                                "correct_answer": "1"
+                            },
+                            {
+                                "id": 14,
+                                "quiz_question_id": 4,
+                                "option_text": "option 2",
+                                "correct_answer": "0"
+                            },
+                            {
+                                "id": 15,
+                                "quiz_question_id": 4,
+                                "option_text": "option 3",
+                                "correct_answer": "0"
+                            },
+                            {
+                                "id": 16,
+                                "quiz_question_id": 4,
+                                "option_text": "option 4",
+                                "correct_answer": "0"
+                            }
+                        ]
+                    },
+                    {
+                        "id": 5,
+                        "course_id": 1,
+                        "module_id": 2,
+                        "question_title": "select your option",
+                        "type": "radio",
+                        "status": "active",
+                        "options": [
+                            {
+                                "id": 17,
+                                "quiz_question_id": 5,
+                                "option_text": "option 1",
+                                "correct_answer": "0"
+                            },
+                            {
+                                "id": 18,
+                                "quiz_question_id": 5,
+                                "option_text": "option 2",
+                                "correct_answer": "1"
+                            },
+                            {
+                                "id": 19,
+                                "quiz_question_id": 5,
+                                "option_text": "option 3",
+                                "correct_answer": "0"
+                            },
+                            {
+                                "id": 20,
+                                "quiz_question_id": 5,
+                                "option_text": "option 4",
+                                "correct_answer": "0"
+                            }
+                        ]
+                    },
+                    {
+                        "id": 6,
+                        "course_id": 1,
+                        "module_id": 2,
+                        "question_title": "What is the",
+                        "type": "radio",
+                        "status": "active",
+                        "options": [
+                            {
+                                "id": 21,
+                                "quiz_question_id": 6,
+                                "option_text": "option 1",
+                                "correct_answer": "0"
+                            },
+                            {
+                                "id": 22,
+                                "quiz_question_id": 6,
+                                "option_text": "option 2",
+                                "correct_answer": "0"
+                            },
+                            {
+                                "id": 23,
+                                "quiz_question_id": 6,
+                                "option_text": "option 3",
+                                "correct_answer": "0"
+                            },
+                            {
+                                "id": 24,
+                                "quiz_question_id": 6,
+                                "option_text": "option 4",
+                                "correct_answer": "1"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+}
